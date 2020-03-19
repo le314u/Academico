4005c9bc4a5590d9653078117ac7862de43fab6c
Lucas Mateus <lucasmfpiu@gmail.com>
2020-03-18 23:10:35 -0300

    Interativo

--- IFMG/InteligenciaArtificial/CacaPalavras/Source/args.py
+++ IFMG/InteligenciaArtificial/CacaPalavras/Source/args.py
@@ -13,11 +13,36 @@ class argumentos():
             self.nCacaPalavras = 1
 
     def arg(self):
+        self.nomeArquivo()
+        self.separador()
+        self.numeroIndividuos()
+        self.numeroGeracoes()
+        self.idadeMaxima()
+        self.nLinhas()
+        self.nColunas()
+        self.nCacaPalavras()
+
+    def nomeArquivo(self):
         self.nomeArquivo = input("Nome arquivo:")
+
+    def separador(self):
         self.separador = input("Delimitador:")
+
+    def numeroIndividuos(self):
         self.numeroIndividuos = int( input("Numero de individuos por geração:") )
+
+    def numeroGeracoes(self):
         self.numeroGeracoes = int( input("Numero de gerações:") )
+
+    def idadeMaxima(self):
         self.idadeMaxima = int( input("Idade máxima do gene:") )
+
+    def nLinhas(self):
         self.nLinhas = int( input("Dimensão Linhas:") )
+
+    def nColunas(self):
         self.nColunas = int( input("Dimensão Colunas:") )
+
+    def nCacaPalavras(self):
         self.nCacaPalavras = int( input("Numero de CaçaPalavras:") )
+    

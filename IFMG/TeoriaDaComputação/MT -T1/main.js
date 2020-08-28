45eb055819ff8008eccfcbae41c3051f4925311b
le314u <lucsmfpiu@gmail.com>
2020-08-28 17:59:01 -0300

    Step

--- IFMG/TeoriaDaComputação/MT -T1/main.js
+++ IFMG/TeoriaDaComputação/MT -T1/main.js
@@ -2,7 +2,6 @@ const mt = require('./mt')
 const cli = require('./cli')
 const input = require('./input');
 const output = require('./output')
-const { read } = require('fs');
 const readline = require('readline');
 
 
@@ -57,25 +56,28 @@ class Main{
         /*  3 funções: 1 cria o arquivo(passa o path do arquivo), outra escreve no arquivo(string a ser escrita)
                        outra fecha o arquivo("sem parametro") */
         // flag indicando se o arquivo ta aberto ou não
-        const helpString = '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
+        const geral = '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
         'Este simulador foi feito na linguagem de programação Javascript, por tal, é necessário instalar o "Node.js" na máquina.\n' +
-        'Sua sintaxe de entrada segue as regras abaixo:\n' +
+        'Sua sintaxe de entrada segue as regras abaixo:\n'+
         '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
         ' $ node <nome do arquivo a ser executado> <opções> <arquivo a ser lido> <string de entrada>\n' +
         '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
-        '-- Entre as opções possíveis, estas são:\n' +
-        '   [ -step <n> ] - Esta flag faz com que o simulador execute "n" computações e pare, mostrando o conteúdo das 3 fitas, dando possibilidades\n' +
+        '-- Entre as opções possíveis, estas são:\n'
+
+        const step = '   [ -step <n> ] - Esta flag faz com que o simulador execute "n" computações e pare, mostrando o conteúdo das 3 fitas, dando possibilidades\n' +
         'para se escrever um novo valor "n" para realizar computações reabrindo o prompt de comandos. Caso seja fornecido o valor "0", a simulação\n' +
         'termina imediatamente. Caso seja fornecido um valor negativo, o simulador passa a executar o programa considerando que se foi escrito a\n' +
         'opção "-resume".\n' +
         '   Ex: $ node main.js -step 3 teste.mt 0101010\n' +
-        '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
-        '   [ -resume ] - Esta flag executa o programa até o fim, computando todos os comandos na ordem em que foram apresentados, mostrando o\n' +
+        '-------------------------------------------------------------------------------------------------------------------------------------------\n' 
+        
+        const resume = '   [ -resume ] - Esta flag executa o programa até o fim, computando todos os comandos na ordem em que foram apresentados, mostrando o\n' +
         'conteúdo das 3 fitas e o resultado da computação, se ACEITOU a string de entrada, ou se a REJEITOU. Esta opção é a padrão e será\n' +
         'executada se nenhuma opção "-step" for fornecida.\n' +
         '   Ex: $ node main.js -resume teste.mt 0101010\n' +
-        '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
-        '   [ -debug <arquivo log> ] - Esta flag produz um relatório mostrando linha por linha as computações executadas, as entradas e saídas\n' +
+        '-------------------------------------------------------------------------------------------------------------------------------------------\n' 
+        
+        const debug = '   [ -debug <arquivo log> ] - Esta flag produz um relatório mostrando linha por linha as computações executadas, as entradas e saídas\n' +
         'dos blocos e afins. O parâmetro <arquivo log> que deve ser fornecido logo após a opção de "debug" indica o arquivo de relatório que\n' +
         'será produzido contendo todas estas informações das computações.\n' +
         '   Ex: $ node main.js -debug export.mt teste.mt 0101010\n' +
@@ -84,60 +86,74 @@ class Main{
         'Utilize sempre que precisar de ajuda!\n' +
         '   Ex: $ node main.js -help\n' +
         '-------------------------------------------------------------------------------------------------------------------------------------------\n'
-        console.log(helpString)
+        
+        const standart = 'Caso nenhuma flag seja passada será considerada a flag resume'
+        '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
+        ' $ node <nome do arquivo a ser executado> <arquivo a ser lido> <string de entrada>\n' +
+        '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
+        '   Ex: $ node main.js teste.mt 0101010\n' +
+        '-------------------------------------------------------------------------------------------------------------------------------------------\n' +
+        console.log( geral+ step+ resume+ debug+ standart )
     }
     resume(){
         // Executa o que deve ser feito com a flag -resume
         while ( this.machine.compute()  ){}
         this.machine.machineState()
     }
-    step(){
-        let steps = 0 // Número de steps
-        let maxSteps = this.machine.controll.maxStep // Variável para controle do número máximo de steps
-        
+    async step(){
+        // Executa o que deve ser feito com a flag -step
+        let inExec = true
+
         // Configurando prompt de comando para o usuário digitar
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
-
         });
-        
-        // Pegando-se estas funções como ArrowFunctions para usá-las em outros contextos
-        const context = () => this.resume()
-        const machineStates = () => this.machine.machineState()
 
-
-        while ( this.machine.controll.hasNextStep() && steps < maxSteps ) {
-            this.machine.machineState()
-            steps++
-
-            console.log(steps)
-            console.log(maxSteps)
-            
-            if ( steps == maxSteps ) {
-                rl.question(">> Quais os próximos steps? ", function(answer) {
-                    const num = parseInt(answer)
-
-                    if ( num < 0 ) { // Executar como se fosse "-resume"
-                        context()
-                        machineStates()
-                        rl.close()
-
-                    } else if ( num == 0 ) { // Encerrar a execução
-                        rl.close();
-
-                    } else if ( num > 0 ) { // Adicionar mais Steps
-                        maxSteps = steps + num
-                        console.log(maxSteps)
-                        context()
-                        machineStates()
-
-                        rl.close()
-                    }
-                });
-            }
+        // Pausa para perguntar o que fazer
+        let breakPoint = ()=>{
+            return new Promise( (resolve, reject)=>{
+                try {
+                    rl.question(">> Quais os próximos steps? ", function(answer) {
+                        let option = parseInt(answer)
+                        resolve(option)
+                    });
+                } catch (error) {
+                    console.log("Argumento Invalido")
+                    reject(error)
+                }
+            })
         }
-        // Executa o que deve ser feito com a flag -step
+        // Computa n Passos
+        let exec = ()=>{
+            inExec = true
+            while ( this.machine.controll.hasNextStep()) {
+                this.machine.compute()//Computa
+                this.machine.controll.nextStep()//Contabiliza um passo
+            }
+            this.machine.machineState()// Printa Estado das Fitas
+            inExec = false
+        }
+
+        while(inExec){
+            if(this.machine.controll.getSpecial() != ''){ // Para a execução
+                inExec = false
+                rl.close();
+                break
+            }
+            exec()// Contabiliza 'n' passos
+            await breakPoint().then((option)=>{
+                // Executar conforme a seleção do fluxo
+                if ( option == -1 || option > 0 ) { 
+                    inExec = true
+                    this.machine.controll.moreSteps(option)
+                    return cli.RESUME
+                }else { // Encerrar a execução
+                    rl.close();
+                    return false
+                }
+            })
+        }
     }
     async debug(){
         // Executa o que deve ser feito com a flag -debug

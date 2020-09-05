395858305b34762066d8366b79d900973eb4f261
Lucas Mateus <lucasmfpiu@gmail.com>
2020-09-05 03:31:04 -0300

    bkp2

--- IFMG/DesenvolvimentoLinux/T2/main.sh
+++ IFMG/DesenvolvimentoLinux/T2/main.sh
@@ -50,7 +50,7 @@ showLine(){
 # Sai do programa caso literal não exista
 hasLiteral(){
 # $1 == <Regex a ser procurado>
-    lLiteral=$(getNumberLineMatch "$1")
+    local lLiteral=$(getNumberLineMatch "$1")
     if [ -z $lLiteral ] # Não existe liteal FATORES:
     then
         echo "Sintaxe incompativel"
@@ -62,8 +62,8 @@ hasLiteral(){
 # Salva o nome do fator em uma variavel Global <fatores>
 saveFactor(){
 # $1 == linha onde o fator é declarado
-    line=$1
-    fator=$(getValueRegex_campo1 "^( )*([a-zA-Z]+([0-9]*[a-zA-Z_]*)*)( )*" $line)|sed 's/ //g'|sed 's/=//g'
+    local line=$1
+    local fator=$(getValueRegex_campo1 "^( )*([a-zA-Z]+([0-9]*[a-zA-Z_]*)*)( )*" $line)|sed 's/ //g'|sed 's/=//g'
     GLOBAL_fatores+=("$fator")
 }
 
@@ -76,7 +76,7 @@ nextLine(){
 #Adiciona 1 ao numero passado
 incrementa(){
 # $1 == <numero>
-    teste=$(echo $1 | grep -v "[^0-9]")
+    local teste=$(echo $1 | grep -v "[^0-9]")
     if [ "-$teste" != "-" ]
     then
         echo $[ $1+1 ]
@@ -87,20 +87,20 @@ incrementa(){
 # Transforma o conteudo da linha 'x' em uma variavel
 createVariable(){
 # $1 == <linha onde é declarada o fato>
-    contentLine=$(showLine $line)
-    express=$(echo $contentLine | sed 's/,/ /g' | sed 's/ = /=/g')
+    local contentLine=$(showLine $line)
+    local express=$(echo $contentLine | sed 's/,/ /g' | sed 's/ = /=/g')
     eval $express
 }
 
 # Le os fatores e transforma em um array e salva seus descritores em uma variavel Global
 fatores(){
     hasLiteral "^FATORES:$" #Se o literal não existe aborta o programa
-    lLiteral=$(getNumberLineMatch "^FATORES:$") # Pega a linha onde deu match
-    line=$(nextLine $lLiteral) # Pega a proxima linha
+    local lLiteral=$(getNumberLineMatch "^FATORES:$") # Pega a linha onde deu match
+    local line=$(nextLine $lLiteral) # Pega a proxima linha
     while [ 1 ]
     do
         # Pega o fator ou seja o <descritor de variavel>
-        fator=$(getValueRegex_campo1 "^( )*([a-zA-Z]+([0-9]*[a-zA-Z_]*)*)( )*=" $line)
+        local fator=$(getValueRegex_campo1 "^( )*([a-zA-Z]+([0-9]*[a-zA-Z_]*)*)( )*=" $line)
         if [ -z $fator ] # Não existe Descritor
         then
             return
@@ -108,29 +108,29 @@ fatores(){
         # Transforma o conteudo da linha X em uma variavel
         createVariable $line
         saveFactor $line
-        line=$(nextLine $line) # Pega a proxima linha
+        local line=$(nextLine $line) # Pega a proxima linha
     done
 }
 
 # Salva a lista de comandos
 saveCommand(){
 # $1 == linha onde o comando é declarado
-    line=$1
-    comand=$(showLine $line)
+    local line=$1
+    local comand=$(showLine $line)
     GLOBAL_comandos+=("$comand")
 }
 
 # Verifica se o comando tem apenas fatores validos
 checkComand(){
 # $1 == <comando> 
-    comando=$1
-    factors=$(echo $comando |tr " " "\n" | grep -o "^\$.*"| sed 's/\$//g') # Pega o fator
-    flagReturn=0
+    local comando=$1
+    local factors=$(echo $comando |tr " " "\n" | grep -o "^\$.*"| sed 's/\$//g') # Pega o fator
+    local flagReturn=0
     for element_X in $factors
     do
         if [ -n "$element_X" ] # so checa se for um fator
         then
-            isValid=0 # Flag começo como falso porem durante a execução por se tornar verdadeira
+            local isValid=0 # Flag começo como falso porem durante a execução por se tornar verdadeira
             for element_Y in ${GLOBAL_fatores[@]}
             do
                 if [ "$element_Y" = "$element_X" ]
@@ -150,10 +150,10 @@ checkComand(){
 # Salva a ordem que as variaveis aparecem no comando
 orderVariableInComand(){
 # $1 == <Comando>
-    comando=$1
+    local comando=$1
     for chunck in $comando;
     do
-        isVariable=$(echo $chunck|grep -o "\$[a-zA-Z]\+\([0-9]*[a-zA-Z]*\)*")
+        local isVariable=$(echo $chunck|grep -o "\$[a-zA-Z]\+\([0-9]*[a-zA-Z]*\)*")
         if [ "-$isVariable" != "-" ]
         then
             #Salva a variavel
@@ -164,17 +164,17 @@ orderVariableInComand(){
 # Le os comandose salva em uma variavel Global GLOBAL_comandos
 comando(){
     hasLiteral "^COMANDO:$" #Se o literal não existe aborta o programa
-    lLiteral=$(getNumberLineMatch "^COMANDO:$") # Pega a linha onde deu match
-    line=$(nextLine $lLiteral) # Pega a proxima linha
+    local lLiteral=$(getNumberLineMatch "^COMANDO:$") # Pega a linha onde deu match
+    local line=$(nextLine $lLiteral) # Pega a proxima linha
     while [ 1 ]
     do
         # Procura o comando == <descritor de variavel>
-        comand=$(showLine $line)
+        local comand=$(showLine $line)
         if [ -z "$comand" ] # Parou de dar Match:
         then
             return
         fi
-        isValid=$(checkComand "$comand")
+        local isValid=$(checkComand "$comand")
         if [ "$isValid" = "1" ]
         then
             saveCommand $line # Salva o comando
@@ -191,7 +191,7 @@ comando(){
 
 # Salva a lista de ensaios na ordem em que é declarado
 saveEnsaio(){
-# $1 == instancia de ensaio
+# $1 == <instancia de ensaio>
     GLOBAL_ensaios+=("$1")
 }
 
@@ -204,7 +204,7 @@ saveOrderVariable(){
 ensaioCompativelComando(){
 # $@ == <ensaio>
     IFS=' ' read -a ensaio <<< $1
-    compativel=0
+    local compativel=0
     if [ ${#GLOBAL_orderVariable[@]} -eq ${#ensaio[@]} ]
     then
         compativel=1
@@ -213,95 +213,107 @@ ensaioCompativelComando(){
         exit
     fi
 }
+
+# Persiste um ensaio no arquivo de saida
+saveInstancia(){
+# $1 == <instancia>
+    GLOBAL_instancia+=("$1")
+    echo "$1" >> $GLOBAL_arquivoOutput # Apagar
+}
+
 #Le a linha da instancia do ensaio e altera caso for *
 especificaInstancias(){
-# $1 == <Ensaio em um formato de vetor >
+# $1 == <string contendo o Ensaio>
 # $2 == <Index do nivel dentro de um ensaio> 
-    IFS=' ' read -a ensaio <<< $1
-    tamanho=${#GLOBAL_orderVariable[*]} # numero de niveis assumidos pelos fatores === quantidade de fatores no comando  
-    ultimoIndex=$[ $tamanho - 1 ]
+    local stringEnsaio=$1
     # Verifico se veio o segundo argumento caso contrario seta como 0
     if [ -z $2 ]
     then
-        index="0"
+        local index="0"
     else
-        index=$2
+        local index=$2
     fi
-echo ""
-echo "----especificaInstancias---- Index=$index   Instancia=$1"
-    #Caso base
+    # Transforma a entrada em um vetor
+    IFS=' ' read -a ensaio <<< $stringEnsaio
+    # quantidade de fatores no comando
+    local tamanho=${#GLOBAL_orderVariable[*]}   
+    local ultimoIndex=$[ $tamanho - 1 ]
+
+    echo ""
+    echo "----especificaInstancias---- Index=$index   Instancia=${ensaio[@]}"
+    # echo "Trabalhando na instancia=${ensaio[@]} [$index]"
+    
+    #Percorro todos os fatores do comando <$A $B $C>
+    for (( i=$index; i < $tamanho; i++ ))
+    do
+        echo -e "Dado o ensaio[$i]==${ensaio[$i]}"
+    	if [ ${ensaio[$i]} = "\"*\"" ] # Se for * altero pelos valores possiveis
+    	then
+            especificaInstancias_Recursiva "$stringEnsaio" "$i"            
+    	fi
+    done
+
+    #Caso base ou seja quando não há mais Fatos a ser desdobrado
     if [ $[ $index ] -gt $[ $ultimoIndex ] ]
     then
         echo "Chegou no caso Base"
-        saveEnsaio $ensaio
+        saveInstancia "${ensaio[@]}"
         return
     fi
-	
-    #Percorro todos os fatores do comando <$A $B $C>
-    for (( i=$index; i < ${#GLOBAL_orderVariable[*]}; i++ ))
-    do
-    	if [ ${ensaio[$i]} = "\"*\"" ] # Se for # altero pelos valores possiveis
-    	then
-            especificaInstancias_Recursiva $i            
-    	fi
-    done
+    saveInstancia "${ensaio[@]}"
 }
-# Persiste um ensaio no arquivo de saida
-salvaInstancia(){
-# $1 == <instancia>
-    instancia=$1 >> $GLOBAL_arquivoOutput
-}
+
 # Subistitui o *
 especificaInstancias_Recursiva(){
-# $1 == <Index do ensaio que esta sendo alterado>
-    echo "----Altera----"
-    index=$[ $1+0 ]
-    #variable = <vetor presente em $FATOR>
-    descritorVariable=$(echo $(echo ${GLOBAL_orderVariable[$index]}|sed 's/\$//g') )
-    eval $( echo Niveis=\$\{$descritorVariable\[\@\]\} )
-    IFS=' ' read -a vetNiveis <<< $Niveis
-    qtdNiveis=${#vetNiveis[@]}
-    echo "QTD VEZES FOR = $qtdNiveis"
-    # passa por todos os niveis<valores> de um fator                
+# $1 == <String com o Ensaio>
+# $2 == <Index do fator dentro do ensaio que esta sendo alterado>
+    local stringEnsaio="$1"
+    local index=$[ $2+0 ]
+
+    # Transforma a stringEnsaio em um vetor
+    IFS=' ' read -a ensaio <<< $stringEnsaio
+
+    # Niveis = <vetor presente em $FATOR>
+        local descritorVariable=$(echo $(echo ${GLOBAL_orderVariable[$index]}|sed 's/\$//g') )
+        eval $( echo Niveis=\$\{$descritorVariable\[\@\]\} )
+        IFS=' ' read -a vetNiveis <<< $Niveis
+        local qtdNiveis=${#vetNiveis[@]}
+
+    # Passa por todos os niveis==<valores> de um fator                
     for (( i=$index; i<$qtdNiveis; i++ ))
     do
-        nivelAtual=${vetNiveis[$i]}
-        echo "Fator ${GLOBAL_orderVariable[$i]} i=$i NIVEL ATUAL:${vetNiveis[$j]}"            	
-        ensaio[$index]=$nivelAtual
+        local nivelAtual=${vetNiveis[$i]}
+        echo -e "Trocarei -> ${vetNiveis[$index]} == ${GLOBAL_orderVariable[$i]}[$i] |do conjunto ${vetNiveis[@]}"
+        echo -e "O que era ensaio ${ensaio[@]} vira:"
+        ensaio[ $(($index)) ]="$nivelAtual"
+        echo -e "\t${ensaio[@]}"
         #Chama uma bifurcação
-        next=$(incrementa $index)
-        echo "Chama Bifurcação passando o que era $index para $next "
+        local next=$(incrementa $index)
         especificaInstancias "$Niveis" "$next"
     done
 }
 
-
-
-#	if [ $i -eq ${#GLOBAL_orderVariable[*]} ]
-#    	then
-#	    	echo ${aux[@]} >> teste.txt
-#    	fi
-
-
 #ensaios
 ensaios(){
     hasLiteral "^ENSAIOS:$" #Se o literal não existe aborta o programa
-    lLiteral=$(getNumberLineMatch "^ENSAIOS:$") # Pega a linha onde deu match
-    line=$(nextLine $lLiteral) # Pega a proxima linha
+    local lLiteral=$(getNumberLineMatch "^ENSAIOS:$") # Pega a linha onde deu match
+    local line=$(nextLine $lLiteral) # Pega a proxima linha
     while [ 1 ]
     do
         # Procura o comando == <descritor de variavel>
-        ensaio=$(getValueRegex_campo1 "^( )*([0-9])+( )*=( )*" $line)        
+        local ensaio=$(getValueRegex_campo1 "^( )*([0-9])+( )*=( )*" $line)        
         if [ -z $ensaio ] # Parou de dar Match:
         then
             return
         fi
         #pega os valores dos ensaior aindacom mascara ou seja '*'
-        ensaioMask=$(showLine $line| sed 's/^ *[0-9]\+ *= *//g'| sed 's/,/ /g'| sed 's/  / /g'| sed 's/ *$//g'| sed 's/\*/"*"/g') # não sei mas passar apenas '*' estava causando u merro inesperado
+        local ensaioMask=$(showLine $line| sed 's/^ *[0-9]\+ *= *//g'| sed 's/,/ /g'| sed 's/  / /g'| sed 's/ *$//g'| sed 's/\*/"*"/g') # não sei mas passar apenas '*' estava causando u merro inesperado
         ensaioCompativelComando "$ensaioMask" # Caso contrário encerra a execução
         #Desdobra '*' em valores possiveis	
         especificaInstancias "$ensaioMask"
         line=$(nextLine $line) # Pega a proxima linha
+        echo "--------------PASSOU PRA PROXIMA-------------"
+        exit
     done
 }
 

43438354ec69334a1e51a1d5c1cf05984d8d150c
Lucas Mateus <lucasmfpiu@gmail.com>
2020-09-10 17:13:09 -0300

    Clear

--- IFMG/TeoriaDaComputação/MT -T1/erro.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Erro  extends Error{
-    constructor(name="Erro genérico", condition="Condição não especificada", ...params){
-        super(...params)
-        super.name = 'Erro '+name
-        super.message = `\nCausa: ${condition}\n`;                
-        this.debug=params
-    }
-}

9572990f5a59e41862c634c707ca5b443b734006
Lucas Mateus <lucasmfpiu@gmail.com>
2020-10-15 19:52:35 -0300

    GUI

--- IFMG/DesenvolvimentoLinux/T6/main.py
+++ IFMG/DesenvolvimentoLinux/T6/main.py
@@ -6,34 +6,84 @@ class Application(tk.Frame):
         self.master = master
         self.master.title("Teste")
         self.master.geometry("600x500")
-        f = self.layoutInsertPessoa()
-        f.place(x=0,y=0)
 
-    def newFrame(self, master=None):
+        canvas = tk.Canvas(master=master)
+        scrollbar = tk.Scrollbar( master , orient = tk.VERTICAL, command = canvas.yview)
+        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.pack()
+
+        
+        #Insert
+        self.line(text='Inserir Tabela Pessoas')
+        insertPessoa = self.layoutInsertPessoa(frame=canvas)
+        #Select
+        self.line(text='Select Tabela Pessoas')
+        selectPessoa = self.layoutSelectPessoa(frame=canvas)
+
+
+
+    def scroll(self, frame=None):
+        scrollbar = tk.Scrollbar(frame)
+        scrollbar.pack( side = tk.RIGHT, fill = tk.Y )
+        return scrollbar
+
+    def newFrame(self, master=None, side='top'):
         """Cria um frame"""
         frame = tk.Frame(master)
-        frame.pack(side='bottom')
+        frame.pack(side=side)
         return frame
 
-    def insert(self, frame, txtLabel, nameInp):
+    def insert(self, frame=None, txtLabel='', nameInp=''):
         """ Cria uma linah com Label + Input"""
         f = self.newFrame(frame)
         #Label
         label = tk.Label(f, text=txtLabel)
-        label.pack(side='left', expand=1)
+        label.pack(side='left')
         #Campo Input
         input = tk.Entry(f, textvar=nameInp)
-        input.pack(side='left', expand=3)
+        input.pack(side='left')
 
-    def layoutInsertPessoa(self):
+    def titulo(self, frame=None, text=''):
+        #Cria um titulo
+        label = tk.Label( frame, text=text)
+        label.pack(side='top')
+    
+    def line(self, frame=None, text=''):
+        #Cria um titulo
+        content = str( str('-'*40)+ text+ str('-'*40) )
+        label = tk.Label(frame, text=content)
+        label.pack(side='top')
+
+    def layoutInsertPessoa(self, frame=None):
         """Cria o Inset da tabela Pessoa"""
-        #Cria o Frame
-        frame = self.newFrame()
-        #Cria N linhas
-        self.insert(frame,"Texto1",1)
-        self.insert(frame,"Texto1",2)
-        #Cria um button
-        return frame
+        #Cria os Frames
+        frameG = self.newFrame(master=frame, side='top')
+        frameL = self.newFrame(master=frameG,side='left')
+        frameR = self.newFrame(master=frameG,side='left')
+        #Cria N linha
+        self.insert(frameL,"Nome\t","nome")
+        self.insert(frameL,"Cpf\t","cpf")
+        self.insert(frameL,"E-mail\t","email")
+        self.insert(frameL,"UF\t","uf")
+        #Cria um botão
+        bt = tk.Button(frameR,text="Teste", padx=50,bg='green')
+        bt.pack(padx=50,pady=50)
+        frameG.pack()
+        return frameG
+
+    def layoutSelectPessoa(self, frame=None):
+        """Cria o Inset da tabela Pessoa"""
+        #Cria os Frames
+        frameG = self.newFrame(master=frame, side='top')
+        frameL = self.newFrame(master=frameG,side='left')
+        frameR = self.newFrame(master=frameG,side='left')
+        #Cria linha de input 
+        self.insert(frameL,"Cpf\t","cpfSelect")
+        #Cria um botão
+        bt = tk.Button(frameR,text="Teste", padx=50,bg='green')
+        bt.pack(padx=50)   
+        frameG.pack()
+        return frameG
 
     def say_hi(self):
         print("hi there, everyone!")

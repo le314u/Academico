970734c503f4ed1e1e8463b6f1407c38dff03d9c
Lucas Mateus <lucasmfpiu@gmail.com>
2020-10-30 00:58:21 -0300

    So falta o Delete

--- IFMG/DesenvolvimentoLinux/T7/main.py
+++ IFMG/DesenvolvimentoLinux/T7/main.py
@@ -1,27 +1,34 @@
 class Tree:
     def __init__(self):
         self.RubroNegra = RubroNegra()
-
-    def __getitem__(self, arg):
-        return "oi"
+    
+    def __getitem__(self, key):
+        key = str(key)
+        return self.RubroNegra.procuraKey(key)
 
     def __setitem__(self, key, value):
-        self[key] = value
+        key = str(key)
+        if value != None:
+            try:
+                self[key].dado = value
+            except:
+                self.insert(key, value)
+        else:
+            pass
+
+    def __iter__(self):
+        return self.inordem()
 
     def insert(self, key, value):
         self.RubroNegra.inserir(key, value)
-
-    def get(self, key):
-        pass
-
     def preordem(self):
         return self.RubroNegra.preordem()
 
     def inordem(self):
-        yield self.RubroNegra.inordem()
+        return self.RubroNegra.inordem()
 
     def posordem(self):
-        yield self.RubroNegra.posordem()
+        return self.RubroNegra.posordem()
 
 
 # DEFINE Tabela de COR
@@ -34,12 +41,15 @@ color = {
 # Strutura de dados que representa um nó dentro da arvore rubro negra
 class No:
     def __init__(self, chave, dado):
-        self.chave = int(chave)  # Identificador do nó
+        self.chave = chave  # Identificador do nó
         self.dado = dado  # Conteudo do nó
         self.pai = None  # Ponteiro para o nó pai
         self.esquerda = None  # Ponteiro para o nó da esquerda
         self.direita = None  # Ponteiro para o nó da direita
         self.cor = color['red']
+
+    def __str__(self):
+        return self.chave + "--->"+self.dado
 
     def vo(self):
         return self.pai.pai
@@ -188,14 +198,37 @@ class RubroNegra:
     def preordem_rec(self, no):
         if (no != self.arvoreVazia):
             yield no
-            self.preordem_rec(no.esquerda)
-            self.preordem_rec(no.direita)
+            yield from (self.preordem_rec(no.esquerda))
+            yield from (self.preordem_rec(no.direita))
 
     def inordem(self):
-        pass
+        return self.inordem_rec(self.raiz)
+
+    def inordem_rec(self, no):
+        if (no != self.arvoreVazia):
+            yield from (self.inordem_rec(no.esquerda))
+            yield no
+            yield from (self.inordem_rec(no.direita))
 
     def posordem(self):
-        pass
+        return self.posordem_rec(self.raiz)
+
+    def posordem_rec(self, no):
+        if (no != self.arvoreVazia):
+            yield from (self.posordem_rec(no.esquerda))
+            yield from (self.posordem_rec(no.direita))
+            yield no
+
+    def procuraKey(self, key):
+        no = self.raiz
+        while no != self.arvoreVazia:
+            if key < no.chave:
+                no = no.esquerda
+            elif key > no.chave:
+                no = no.direita
+            elif key == no.chave:
+                return no
+        return None
 
 
 
@@ -208,8 +241,27 @@ if __name__ == "__main__":
     bst.insert("17", "")
     bst.insert("25", "")
     bst.insert("40", "")
-    bst.insert("80", "")
-    for i in bst.preordem():
+    bst.insert("80", "Alguma Coisa")
+
+    print(bst["80"])
+    print(bst[80])
+    print(bst["80"].dado)
+    bst["80"]= "teste"
+    print(bst["80"].dado)
+    bst["800"]= "teste"
+    for i in bst:
         print(i)
+
+    #for i in bst.preordem():
+    #    print(i.chave)
+
+    #for i in bst.inordem():
+    #    print(i.chave)
+
+    #for i in bst.posordem():
+    #    print(i.chave)
+
+    #for i in bst:
+    #    print(i.chave)
+
     # bst.delete_no(25)
-    # bst.pretty_print()
